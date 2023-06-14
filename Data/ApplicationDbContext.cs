b3351314c21c904d0f9e371e98688ca6080430a5
Mateusz Kowalski <236567@edu.p.lodz.pl>
2023-06-14 23:24:56 +0200

    feat: Migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ASP.NET_Core_MVC_Application.Models;
+
+namespace ASP.NET_Core_MVC_Application.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Game> Games { get; set; }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_Core_MVC_Application.Models;
+
+public enum GameGenre
+{
+    Action,
+    Adventure,
+    RPG,
+    Simulation,
+    Strategy,
+    Sports,
+    Racing,
+    Fighting,
+    Horror,
+    Puzzle,
+    Idle,
+    Other
+}
+
+public class Game
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    public GameGenre Genre { get; set; }
+    public string? Description { get; set; }
+}
