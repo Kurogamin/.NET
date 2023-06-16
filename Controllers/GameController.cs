3d81a9e8ee15e071b3b5091cbf158f9c349093fa
Mateusz Kowalski <236567@edu.p.lodz.pl>
2023-06-16 17:17:08 +0200

    feat: bootstrap

---
+++ Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_Core_MVC_Application.Data;
+using ASP.NET_Core_MVC_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_MVC_Application.Controllers;
+public class GameController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public GameController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Game> games = _dbContext.Games;
+        return View(games);
+    }
+}
